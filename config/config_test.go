6d97baf1789837fc75cacea210276df8840770d1
Brendan McMillion <brendan@cloudflare.com>
2019-02-04 12:36:22 -0800

    Add .travis.yml.

--- config/config_test.go
+++ config/config_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestStructLogConfig(t *testing.T) {
-	want := `configpb.LogConfig{LogId:0, Prefix:"", OverrideHandlerPrefix:"", RootsPemFile:[]string(nil), PrivateKey:(*any.Any)(nil), PublicKey:(*keyspb.PublicKey)(nil), RejectExpired:false, ExtKeyUsages:[]string(nil), NotAfterStart:(*timestamp.Timestamp)(nil), NotAfterLimit:(*timestamp.Timestamp)(nil), AcceptOnlyCa:false, LogBackendName:"", IsMirror:false, MaxMergeDelaySec:0, ExpectedMergeDelaySec:0, FrozenSth:(*configpb.SignedTreeHead)(nil), XXX_NoUnkeyedLiteral:struct {}{}, XXX_unrecognized:[]uint8(nil), XXX_sizecache:0}`
+	want := `configpb.LogConfig{LogId:0, Prefix:"", OverrideHandlerPrefix:"", RootsPemFile:[]string(nil), PrivateKey:(*any.Any)(nil), PublicKey:(*keyspb.PublicKey)(nil), RejectExpired:false, RejectUnexpired:false, ExtKeyUsages:[]string(nil), NotAfterStart:(*timestamp.Timestamp)(nil), NotAfterLimit:(*timestamp.Timestamp)(nil), AcceptOnlyCa:false, LogBackendName:"", IsMirror:false, MaxMergeDelaySec:0, ExpectedMergeDelaySec:0, FrozenSth:(*configpb.SignedTreeHead)(nil), XXX_NoUnkeyedLiteral:struct {}{}, XXX_unrecognized:[]uint8(nil), XXX_sizecache:0}`
 	cand := fmt.Sprintf("%#v", configpb.LogConfig{})
 
 	if want != cand {
